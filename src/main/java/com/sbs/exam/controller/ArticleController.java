3c4614aca5b81d4427b49816f6413099cc1aafac
SangWon7242 <gotkdqja@gmail.com>
2022-10-06 21:14:14 +0900

    Rq클래스에 jsp 연결 기능 구현

--- src/main/java/com/sbs/exam/controller/ArticleController.java
+++ src/main/java/com/sbs/exam/controller/ArticleController.java
@@ -1,19 +1,15 @@
 package com.sbs.exam.controller;
 
+import com.sbs.exam.Rq;
 import com.sbs.exam.dto.Article;
 import com.sbs.exam.service.ArticleService;
-import com.sbs.exam.util.DBUtil;
-import com.sbs.exam.util.SecSql;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.util.List;
-import java.util.Map;
 
-public class ArticleController {
+public class ArticleController extends Controller {
 
   private HttpServletRequest req;
   private HttpServletResponse resp;
@@ -27,7 +23,16 @@ public class ArticleController {
     articleService = new ArticleService(con);
   }
 
-  public void actionList() throws ServletException, IOException {
+  @Override
+  public void performAction(Rq rq) {
+    switch (rq.getActionMethodName()) {
+      case "list":
+        actionList(rq);
+        break;
+    }
+  }
+
+  public void actionList(Rq rq) {
     int page = 1;
 
     if(req.getParameter("page") != null && req.getParameter("page").length() != 0) {
@@ -40,6 +45,7 @@ public class ArticleController {
     req.setAttribute("articles", articles);
     req.setAttribute("page", page);
     req.setAttribute("totalPage", totalPage);
-    req.getRequestDispatcher("/jsp/article/list.jsp").forward(req, resp);
+
+    rq.jsp("article/list");
   }
 }
